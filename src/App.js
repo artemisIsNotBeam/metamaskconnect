e5566e4834f3e675f2e404d4bb3ad72c4c159c07
FPS man <66878297+artemisIsNotBeam@users.noreply.github.com>
2022-03-26 06:35:56 -0700

    my tutorials use

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Wallet from './Wallet.js'
-import WalletPar from './WalletParent.js'
+import WalletCard from './WalletCard';
+import WalletCardEthers from'./WalletCardEthers';
 
 function App() {
+
   return (
     <div className="App">
-      <h1>why am I working?</h1>
-      <WalletPar/>
+    <WalletCard/>
+    <WalletCardEthers/>
     </div>
   );
 }
---
+++ src/WalletCardEthers.js
@@ -0,0 +1,48 @@
+import React, {useState, useEffect} from 'react'
+import {ethers} from 'ethers'
+import './WalletCard.css'
+
+const WalletCardEthers = () => {
+
+	const [errorMessage, setErrorMessage] = useState(null);
+	const [defaultAccount, setDefaultAccount] = useState(null);
+	const [userBalance, setUserBalance] = useState(null);
+	const [connButtonText, setConnButtonText] = useState('Connect Wallet');
+	const [provider, setProvider] = useState(null);
+
+	const connectWalletHandler = () => {
+		if (window.ethereum && defaultAccount == null) {
+			// set ethers provider
+			setProvider(new ethers.providers.Web3Provider(window.ethereum));
+
+			// connect to metamask
+			window.ethereum.request({ method: 'eth_requestAccounts'})
+			.then(result => {
+				setConnButtonText('Wallet Connected');
+				setDefaultAccount(result[0]);
+			})
+			.catch(error => {
+				setErrorMessage(error.message);
+			});
+
+		} else if (!window.ethereum){
+			console.log('Need to install MetaMask');
+			setErrorMessage('Please install MetaMask browser extension to interact');
+		}
+	}
+
+useEffect(() => {
+	if(defaultAccount){
+	provider.getBalance(defaultAccount)
+	.then(balanceResult => {
+		setUserBalance(ethers.utils.formatEther(balanceResult));
+	})
+	};
+}, [defaultAccount]);
+	
+	return (
+		<div></div>
+	);
+}
+
+export default WalletCardEthers;
