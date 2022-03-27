24eb645e07b00745bff585e6b4342180162debd2
FPS man <66878297+artemisIsNotBeam@users.noreply.github.com>
2022-03-27 07:47:51 -0700

    updated Read me and installed web3

--- README.md
+++ README.md
@@ -12,3 +12,6 @@ In the project directory, you can run:
 
 ### `npm start`
 
+get npm install web3
+also npm insall --save ethers
+
--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 import WalletCard from './WalletCard';
+import { ConstructorFragment } from 'ethers/lib/utils';
+import { Wallet } from 'ethers';
+import React from 'react';
 
-function App() {
+import ERC20TransferABI from './Contract/contractABI';
 
-  return (
-    <div className="App">
-    <WalletCard/>
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.contractIntergration = this.contractIntergration.bind(this);
+    this.setState({ 
+      connect:false,
+      contract:"0x6b175474e89094c44da98b954eedeac495271d0f",
+      web3: new Web3('http://localhost:8545')
+    });
+  }
+
+  contractIntergration(){
+    
+  }
+
+  render(){
+    return <div className='App'>
+      <WalletCard /> 
+      <h4>I"m your nice dapp</h4>
+      <p>Nice dapp infomation</p>
+      <button onClick={this.contractIntergration}>Thingy</button>
     </div>
-  );
+  }
 }
 
 export default App;
